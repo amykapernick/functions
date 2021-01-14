ac47215008f90f9494be55f6e1eb01cd5744384d
Amy Kapernick <amy@kapers.dev>
2021-01-14 12:41:28 +0800

    pushing to standalone azure function

---
+++ graphql/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const {ApolloServer, gql} = require('apollo-server-azure-functions')
+const {GraphQLScalarType} = require('graphql')
+const {Kind} = require('graphql/language')
+
+const uuid = require('../utils/uuid')
+const {tasks, sections, events, notes} = require('../utils/fetchData')
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+
+
+const server = new ApolloServer({
+	typeDefs, 
+	resolvers,
+	introspection: true,
+	playground: true,
+	context: ({req}) => {
+		const fakeUser = {
+			userId: 'userId'
+		}
+		return ({
+			...fakeUser
+		})
+	}
+})
+
+module.exports = server.createHandler();
